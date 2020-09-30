d833333a730d6e098afdd52b0524cbbb1abf371d
Crowdbotics <team@crowdbotics.com>
2020-09-30 02:23:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/drew_yard_services_20862/MainActivity.java
@@ -0,0 +1,15 @@
+package com.drew_yard_services_20862;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "drew_yard_services_20862";
+    }
+}
